3cc8dbf2d329c289fc106d093e3098bfc12d9cee
Avinash Nidadavolu <avinash99n@gmail.com>
2022-11-05 13:43:30 -0400

    a7 redux fa22 - Done

--- src/tuiter/profile/index.js
+++ src/tuiter/profile/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {useSelector,useDispatch} from "react-redux";
+import {useSelector} from "react-redux";
 import "./profile.css";
 import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
