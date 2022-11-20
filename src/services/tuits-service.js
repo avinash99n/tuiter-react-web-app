9c5b9bb84bb5cf8e481617432a2af40671cf0d0c
Avinash Nidadavolu <avinash99n@gmail.com>
2022-11-20 17:07:25 -0500

    a8 rest fa22

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -26,6 +26,7 @@ export const deleteTuit = async (tid) => {
 export const updateTuit = async (tuit) => {
     const response = await axios
         .put(`${TUITS_API}/${tuit._id}`, tuit);
+
     return tuit;
 
 }
--- src/tuiter/reducers/tuit-reducer.js
+++ src/tuiter/reducers/tuit-reducer.js
@@ -8,20 +8,7 @@ const initialState = {
     loading: false
 }
 
-const currentUser = {
-    "userName": "NASA",
-    "handle": "@nasa",
-    "image": "/images/spacex.png",
-};
-const templateTuit = {
-    ...currentUser,
-    "topic": "Space",
-    "time": "2h",
-    "liked": false,
-    "replies": 0,
-    "retuits": 0,
-    "likes": 0,
-}
+
 
 const tuitSlice = createSlice({
     name: 'tuits',
